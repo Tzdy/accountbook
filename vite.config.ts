422c68b43f086055089db7707a7503c40dfd3c72
Tsdy <571219188@qq.com>
2022-11-16 23:06:11 +0800

    feat: 增加vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 import legacy from "@vitejs/plugin-legacy";
+import { VitePWA } from "vite-plugin-pwa";
 import Unocss from "unocss/vite";
 import presetWind from "@unocss/preset-wind";
 
@@ -29,6 +30,47 @@ export default defineConfig({
     }),
     Unocss({
       presets: [presetWind()],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo-192x192.png, logo-512x512.png"],
+      workbox: {
+        runtimeCaching: [
+          {
+            // 根据正则表达式进行缓存，如果你喜欢 也可以使用/.*/i
+            urlPattern: /.*/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "asoulcnki-api-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 1.5,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "记账本",
+        short_name: "记账本",
+        description: "平平无奇的记账本",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   resolve: {
